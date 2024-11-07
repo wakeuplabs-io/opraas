4863b954b158e13b2147e839cbc200eeebb4f4dc
matzapata <matuzapata@gmail.com>
2024-11-07 20:01:41 +0700

    cleanup git utils file

--- opraas_core/src/utils/git.rs
+++ opraas_core/src/utils/git.rs
@@ -55,48 +55,16 @@ impl GitReleaseDownloader for Git {
 }
 
 fn download_and_extract(url: &str, destination: &str) -> Result<(), Box<dyn std::error::Error>> {
-    // Fetch the response body
     let response = reqwest::blocking::get(url)?;
 
-    // Collect the entire response body into a Vec<u8>
     let bytes = response.bytes()?;
 
-    // Write the file to disk unzipped
     let target = Path::new(destination);
     if !target.exists() {
         fs::create_dir_all(target)?;
     }
 
-    // Extract the zip contents
     zip_extract::extract(Cursor::new(bytes), &target, true)?;
 
     Ok(())
 }
-
-// let url = format!("{}/archive/refs/tags/{}.zip", release_url, release_tag);
-// let mut response = self.client.get(&url).send()?;
-
-// // Use a stream to read the response body
-// let mut bytes = Vec::new();
-// let chunk_size = 1024;
-
-// loop {
-//     let mut buffer = vec![0; chunk_size];
-//     let bcount = response
-//         .read_vectored(&mut [io::IoSliceMut::new(&mut buffer)])
-//         .unwrap();
-//     buffer.truncate(bcount);
-
-//     if !buffer.is_empty() {
-//         bytes.extend(buffer.into_boxed_slice().into_vec().iter().cloned());
-//     } else { break; }
-// }
-
-// // Write the file to disk unzipped
-// let target = Path::new(destination);
-// if !target.exists() {
-//     std::fs::create_dir_all(target)?;
-// }
-// zip_extract::extract(Cursor::new(bytes), &target, true)?;
-
-// Ok(())
