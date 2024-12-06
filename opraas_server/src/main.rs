08902dc02824a3a34a0cc4df9ecfd3068dbb5bb4
matzapata <matuzapata@gmail.com>
2024-12-06 16:01:54 +0800

    logging

--- opraas_server/src/main.rs
+++ opraas_server/src/main.rs
@@ -1,19 +1,29 @@
-use std::net::SocketAddr;
-
 mod handlers;
 mod routes;
 mod utils;
 
+use std::net::SocketAddr;
+use tower_http::trace::{self, TraceLayer};
+use tracing::Level;
+
 #[tokio::main]
 async fn main() {
-    env_logger::init();
-
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     println!("Listening on {}", addr);
 
-    let app = routes::configure();
+    tracing_subscriber::fmt()
+        .with_target(false)
+        .compact()
+        .init();
+
+    let router = routes::configure().layer(
+        TraceLayer::new_for_http()
+            .make_span_with(trace::DefaultMakeSpan::new().level(Level::INFO))
+            .on_response(trace::DefaultOnResponse::new().level(Level::INFO)),
+    );
+
     axum::Server::bind(&addr)
-        .serve(app.into_make_service())
+        .serve(router.into_make_service())
         .await
         .unwrap();
 }
